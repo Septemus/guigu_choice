c92cabf33af429134a9687975d3716449e622ecf
joe <musketeerdt@gmail.com>
2023-12-21 21:26:00 +0800

    feat: homepage start

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,9 +1,22 @@
 <template>
-  <div>
-    <h1>this is homepage</h1>
+  <div class="common-layout">
+    <el-container>
+      <el-aside width="200px">Aside</el-aside>
+      <el-main>Main</el-main>
+    </el-container>
   </div>
 </template>
 
 <script lang="ts" setup></script>
 
-<style lang="scss"></style>
+<style lang="scss" scoped>
+.el-container {
+  height: 100vh;
+
+  .el-aside {
+    width: 260px;
+    height: 100%;
+    background-color: $homeSideBgColor;
+  }
+}
+</style>
